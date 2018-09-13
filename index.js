b9b8abe14d00e9dd5a05a53c64a62ece6ea981cf
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-09-13 18:44:29 +0900

    expose async function
    [BREAKING] drop support for Node.js < 7

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 const ghGet = require('gh-get');
 
-module.exports = function ghPost(...args) {
+module.exports = async function ghPost(...args) {
 	const argLen = args.length;
 
 	if (argLen !== 2) {
-		return Promise.reject(new RangeError(`Expected 2 arguments (<string>, <Object>), but got ${
+		throw new RangeError(`Expected 2 arguments (<string>, <Object>), but got ${
 			argLen === 0 ? 'no' : argLen
-		} arguments.`));
+		} arguments.`);
 	}
 
 	return ghGet(args[0], Object.assign({}, args[1], {method: 'POST'})); // eslint-disable-line prefer-object-spread
